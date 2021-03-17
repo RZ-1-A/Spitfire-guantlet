bc877924b3a7e095bc9d4b879f1f83087504b01c
RZ-1-A <79218758+RZ-1-A@users.noreply.github.com>
2021-03-17 12:49:10 -0400

    Update person.js

--- person.js
+++ person.js
@@ -1,13 +1,13 @@
-
 function Person(img) {
-  img = loadImage('spitfiregood.png')
-  this.pos = createVector(10, height - 50);
-  this.vel = createVector(3, 0);
+  img = loadImage('b21super.png')
+  this.pos = createVector(-11101, height - 50);
+  this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
   this.pic = img;
   var ded = 0;
   var highScore = 500;
-
+  this.planes = ded;
+  // this.mass = m;
 
   this.applyForce = function(force) {
     this.acc.add(force);
@@ -25,9 +25,9 @@ function Person(img) {
     stroke(255);
     image(this.pic, this.pos.x + 20, this.pos.y - 9, 100, 60);
     textSize(18)
-    text('Deaths: ' + ded, this.pos.x + (canvasX-100), 20)
+    text('Planes Lost: ' + ded, this.pos.x + (canvasX-150), 20)
     var score = this.pos.x-300;
-
+    if (score <0){score=0}
        if (score >= 9745){  
  score = (highScore + score)
       }
@@ -35,14 +35,31 @@ function Person(img) {
     text('Score: '+score,this.pos.x+(canvasX-120), 40)
      text('High Score: '+highScore,this.pos.x+(canvasX-160), 60)
 
-      }
+
+  }
 
   this.hit = function(ammo) {
-    if ((ammo.pos.x >= (this.pos.x + 5) && ammo.pos.x <= (this.pos.x + 100)) && (ammo.pos.y >= (this.pos.y + 3) && ammo.pos.y <= (this.pos.y + 50))) {
+    if ((ammo.pos.x >= (this.pos.x + 5) && ammo.pos.x <= (this.pos.x + 100)) && (ammo.pos.y >= (this.pos.y + 3) && ammo.pos.y <= (this.pos.y + 48))) {
       ammo.pos.y = -400;
-
+      // fill(0, 0, 0);
+      // rect(0,0,4000,4000);
+      // fill(255, 0, 0);
+      // text('this.pos.y',this.pos.x+150, 200)
+      //translate(1000,1000)
       this.pos.x = (0 - 11000);
       ded++;
+      if(ded >= 25){
+      this.vel.x=0
+   this.vel.y=0
+   fill(0,0,0);
+     rect(0,0,10000,10000)
+    fill(255,0,0)
+     text('The Battle Is Lost',man.pos.x+100, 200)
+   ded=0
+    highScore=500 
+     this.pos.x = -1100100
+      
+      }
     }
 
 
@@ -66,7 +83,5 @@ function Person(img) {
       this.pos.y = 10;
     }
   }
-
-
 
 }
