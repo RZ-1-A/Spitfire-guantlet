869c68a545cfb3059355f4de80554b2d26a5c7b6
RZ-1-A <79218758+RZ-1-A@users.noreply.github.com>
2021-03-17 11:40:35 -0400

    Update skecth.js

--- skecth.js
+++ skecth.js
@@ -3,86 +3,107 @@
  let img;
  let ammo = [];
  let button;
-var canvasX = 500;
-var canvasY = 400;
+ var canvasX = 500;
+ var canvasY = 400;
+ let sira;
 
 
 
  function setup() {
    createCanvas(canvasX, canvasY);
    this.pos = createVector(10, height - 50);
-   img = loadImage('spitfiregood.png');
+   img = loadImage('b21super.png');
    img2 = loadImage('bullte.png');
-   img3 = loadImage('battle.png');
+   img3 = loadImage('battle.jpg');
    man = new Person(img);
    button = createButton('restart');
-   button.position(canvasX/2, man.pos.y + 55);
+   button.position(canvasX / 2, man.pos.y + 55);
    button.mousePressed(restart);
    for (var i = 0; i < 10100; i = i + 5) {
      ammo[i] = new
-     Bullet(img2, random(300, 10000), random(10, 380));
-   }
+     Bullet(img2, random(300, 10000), random(15, (canvasY - 20)));
 
+   }
 
  }
 
 
- function keyPressed() {
-   if (key == ' ') {
-     let jump = createVector(0, -4);
-     man.applyForce(jump)
-   }
- }
+ 
 
  function restart() {
    man.pos.x = 10;
-   man.pos.y = 350
-   
+   man.pos.y = 350;
+  
+
 
  }
 
+
  function draw() {
 
-   if (man.pos.x <= -10) {
+   if (man.pos.x <= -10 && man.pos.x >= -11000) {
      fill(0, 0, 0);
      rect(0, 0, canvasX, canvasY);
      fill(255, 0, 0);
      textSize(50);
-     text('Game Over', canvasX/3-15, 220);
+     text('Game Over', canvasX / 3 - 15, 220);
      textSize(50);
 
    } else {
      background(img3);
-     if (mouseIsPressed) {
-       man.vel.y = 0.04;
+ 
 
 
-     } else {
-       var gravity = createVector(0, 0.1);
-       man.applyForce(gravity);
-
+     if (keyIsDown(65)) {
+       man.vel.x = 1;
      }
 
 
+     if (keyIsDown(68)) {
+       man.vel.x = 3;
+     }
+
+     if (keyIsDown(83)) {
+       man.pos.y += 3;
+     }
+     if (keyIsDown(87)) {
+       man.pos.y -= 3;
+     }
+
      translate(-man.pos.x, 0);
-
-
-    // text(man.pos.x, man.pos.x , man.pos.y )
 
      man.update();
      man.edges();
      man.display();
 
+     if (man.pos.x <= -11000) {
+       fill(255, 255, 255)
+       rect(man.pos.x, 0, canvasX, canvasY)
+       sira = createButton('start');
+       sira.position(man.pos.x + 100, man.pos.y - 50);
+       sira.mousePressed(restart);
+       man.vel.x = 0
+       fill(0, 0, 0)
+       textSize(25)
+       text('Press the spacebar to jump', man.pos.x + 10, 240)
+       text('Press the left mouse button to glide', man.pos.x + 90, 160)
+
+     }
+
+
+
+
+
      for (var i = 0; i < 10100; i = i + 60) {
 
        ammo[i].update();
        ammo[i].display();
-       //  ammo[i].edges();
        man.hit(ammo[i]);
 
      }
-    man.endless(ammo[i]);
-     
+     man.endless(ammo[i]);
+
    }
 
  }
+
